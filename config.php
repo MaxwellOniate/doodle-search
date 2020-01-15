74f078442e570229d9312730058419329eba085d
maxwelloniate <maxwelloniate@gmail.com>
2020-01-15 14:47:09 -0500

    updated config

--- config.php
+++ config.php
@@ -2,8 +2,13 @@
 
 ob_start();
 
+$host = "us-cdbr-iron-east-05.cleardb.net";
+$dbName = "heroku_ef2e315d80d3684";
+$user = "bcdf1ea6907400";
+$pass = "9a2fdc80";
+
 try {
-  $con = new PDO("mysql:dbname=doodle;host=localhost", "root", "pass");
+  $con = new PDO("mysql:dbname=$dbName;host=$host", "$user", "$pass");
   $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
   echo 'Connection failed: ' . $e->getMessage();
