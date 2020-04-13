d618d73692ab32a3e3f61a519b519389ff6e1471
maxwelloniate <maxwelloniate@gmail.com>
2020-04-13 14:11:24 -0400

    Config

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 ob_start();
 
-$host = "us-cdbr-iron-east-05.cleardb.net";
-$dbName = "heroku_ef2e315d80d3684";
-$user = "bcdf1ea6907400";
-$pass = "9a2fdc80";
+$host = "localhost";
+$dbName = "doodle";
+$user = "root";
+$pass = "";
 
 try {
   $con = new PDO("mysql:dbname=$dbName;host=$host", "$user", "$pass");
